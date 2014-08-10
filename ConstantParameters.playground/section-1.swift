d13aa51daf395f3f8250733a49e1c608c0350f4f
Bob Martens <bob@bobmartens.net>
2014-08-09 23:17:30 -0500

    through Section 5.6

--- ConstantParameters.playground/section-1.swift
+++ ConstantParameters.playground/section-1.swift
@@ -1,19 +1,12 @@
 import Foundation
 var coffeeDrinks = ["Drip", "Espresso", "Americano", "Cappuccino", "Latte"]
 
-func emphasize(inout theContentsOf array:[String], byApplying modification:(String) -> String) -> [String] {
+func emphasize(inout array:[String], modification:(String) -> String) -> [String] {
     for i in 0 ..< array.count {
         array[i] = modification(array[i])
     }
     return array
 }
 
-func upperCase(phrase:String) -> String {
-    return phrase.uppercaseString
-}
-
-emphasize(theContentsOf: &coffeeDrinks, byApplying: upperCase)
-coffeeDrinks
-
-emphasize(theContentsOf: &coffeeDrinks, byApplying: {(phrase:String) -> String in return phrase + "!"})
+emphasize(&coffeeDrinks) {$0 + "!"}
 coffeeDrinks
