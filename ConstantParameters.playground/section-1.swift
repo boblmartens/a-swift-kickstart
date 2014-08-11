4d8c6daacd894afb7a61a1c02e32d0629004401b
Bob Martens <bob@bobmartens.net>
2014-08-10 22:46:04 -0500

    through Section 5.8

--- ConstantParameters.playground/section-1.swift
+++ ConstantParameters.playground/section-1.swift
@@ -1,7 +1,8 @@
 import Foundation
 var coffeeDrinks = ["Drip", "Espresso", "Americano", "Cappuccino", "Latte"]
+var numbers = [5, 1, 4, 8, 2, 7]
 
-func emphasize(inout array:[String], modification:(String) -> String) -> [String] {
+func emphasize<T>(inout array:[T], modification:(T) -> T) -> [T] {
     for i in 0 ..< array.count {
         array[i] = modification(array[i])
     }
@@ -10,3 +11,6 @@ func emphasize(inout array:[String], modification:(String) -> String) -> [String
 
 emphasize(&coffeeDrinks) {$0 + "!"}
 coffeeDrinks
+
+emphasize(&numbers) {number in number * 100}
+numbers
