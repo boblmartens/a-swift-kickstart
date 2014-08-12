ea4bde361aa5fb227e66e68a414e0faa66addd19
Bob Martens <bob@bobmartens.net>
2014-08-12 11:30:59 -0500

    completed 6.1

---
+++ Enumerations.playground/section-1.swift
@@ -0,0 +1,12 @@
+enum BasketballPosition {
+    case PointGuard, ShootingGuard
+    case SmallForward, PowerForward
+    case Center
+}
+
+var ballHandler = BasketballPosition.PointGuard
+ballHandler = .SmallForward
+
+var shooter:BasketballPosition
+
+shooter = .PowerForward
