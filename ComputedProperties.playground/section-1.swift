259560978629e4df1555b3bab459fb3286cc4d57
Bob Martens <bob@bobmartens.net>
2014-08-14 22:50:18 -0500

    through 6.10

--- ComputedProperties.playground/section-1.swift
+++ ComputedProperties.playground/section-1.swift
@@ -1,3 +1,7 @@
+protocol PlayerDescribable {
+    func playerDescription() -> String
+}
+
 enum BasketballPosition {
     case PointGuard(Int, String), ShootingGuard(Int, String)
     case SmallForward(Int, String), PowerForward(Int, String)
@@ -32,15 +36,32 @@ enum BasketballPosition {
             return playerName
         }
     }
-    
+}
+
+extension BasketballPosition:PlayerDescribable {
     func playerDescription() -> String {
         return "The ball handler is number \(self.uniformNumber), \(self.playerName)."
     }
 }
 
 var ballHandler = BasketballPosition.PowerForward(21, "Tim Duncan")
-func commentary (position:BasketballPosition) -> String {
+func commentary (position:PlayerDescribable) -> String {
     return position.playerDescription()
 }
 
 let colorCommentary = commentary(ballHandler)
+
+enum HockeyPosition: String {
+    case Goalie = "goalie"
+    case LeftDefenseman = "left d", RightDefenseman = "right d"
+    case LeftWing = "left wing", RightWing = "right wing", Center = "center"
+}
+
+extension HockeyPosition: PlayerDescribable {
+    func playerDescription() -> String {
+        return "The puck handler is the \(self.toRaw())."
+    }
+}
+
+var puckHandler = HockeyPosition.RightDefenseman
+let hockeyCommentary = commentary(puckHandler)
