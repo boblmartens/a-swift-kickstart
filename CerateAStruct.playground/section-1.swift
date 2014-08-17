cf8d94c7b617e9506f449788f69e99079e7875a4
Bob Martens <bob@bobmartens.net>
2014-08-16 22:32:02 -0500

    through section 7.2

--- CerateAStruct.playground/section-1.swift
+++ CerateAStruct.playground/section-1.swift
@@ -1,6 +1,12 @@
 struct Vertex {
-    
+    let x = 3.0
+    let y = 4.0
 }
 
-let point = Vertex()
+let point = Vertex(x: 5.0, y: 6.0)
 point
+point.x
+point.y
+
+let pointOne = Vertex()
+let pointTwo = Vertex(x: 5.0, y: 6.0)
