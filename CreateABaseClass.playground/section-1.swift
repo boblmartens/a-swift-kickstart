500464341a9afda455cd64bd6d124d9d9032ee1c
Bob Martens <bob@bobmartens.net>
2014-08-20 20:16:08 -0500

    through 8.2

--- CreateABaseClass.playground/section-1.swift
+++ CreateABaseClass.playground/section-1.swift
@@ -1,5 +1,17 @@
 class BasketballTeam {
+    let name, affiliation: String
     
+    init(name:String, affiliation:String) {
+        self.name = name
+        self.affiliation = affiliation
+    }
+    
+    func colorCommentary() -> String {
+        return "The \(affiliation) \(name)"
+    }
 }
 
-var team = BasketballTeam()
+let jazz = BasketballTeam(name: "Jazz", affiliation: "Utah")
+jazz.colorCommentary()
+let buckeyes = BasketballTeam(name: "Buckeyes", affiliation: "Ohio State")
+buckeyes.colorCommentary()
