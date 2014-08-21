cc12652fc3e430a58958fdf2ca1695f18ce2b174
Bob Martens <bob@bobmartens.net>
2014-08-20 22:19:34 -0500

    through 8.7

--- CreateABaseClass.playground/section-1.swift
+++ CreateABaseClass.playground/section-1.swift
@@ -4,8 +4,10 @@ class BasketballTeam {
         self.init(name:name, affiliation:"Intramural Team")
     }
     init(name:String, affiliation:String) {
+        println("designated initializer before property inits in BasketballTeam")
         self.name = name
         self.affiliation = affiliation
+        println("designated initializer after property inits in BasketballTeam")
     }
     
     func colorCommentary() -> String {
@@ -16,11 +18,15 @@ class BasketballTeam {
 class ProfessionalBasketballTeam: BasketballTeam {
     let league: String
     convenience init() {
+        println("convenience initializer before self.init in ProfessionalBasketballTeam")
         self.init(name:"Globetrotters", affiliation:"Harlem", league:"Independent League")
+        println("convenience initializer after self.init in ProfessionalBasketballTeam")
     }
     init(name: String, affiliation: String, league: String) {
+        println("designated initializer before self.init in ProfessionalBasketballTeam")
         self.league = league
         super.init(name: name, affiliation: affiliation)
+        println("designated initialzier after self.init in ProfessionalBasketballTeam")
     }
     override func colorCommentary() -> String {
         return super.colorCommentary() + " of the \(league)"
@@ -43,3 +49,6 @@ relocatedJazz.colorCommentary()
 
 let buckeyes = CollegeBasketballTeam(name: "Buckeyes", affiliation: "Ohio State", conference: "Big 10")
 buckeyes.colorCommentary()
+
+let trotters = ProfessionalBasketballTeam()
+trotters.colorCommentary()
